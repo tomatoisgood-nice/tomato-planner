c9f996ba8d7fa746342a0f392dfd1dc2508e40bf
tomatoisgood-nice <hb202410032@gmail.com>
2026-06-14 21:34:23 +0900

    Update notion.js

--- netlify/functions/notion.js
+++ netlify/functions/notion.js
@@ -1,9 +1,36 @@
+const { Client } = require("@notionhq/client");
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
 exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      success: true,
-      message: "Tomato Planner API 연결 성공 🍅"
-    })
-  };
+  try {
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID,
+    });
+
+    const tasks = response.results.map((page) => ({
+      id: page.id,
+      task:
+        page.properties.Task?.title?.[0]?.plain_text ||
+        "Untitled",
+      done:
+        page.properties.Done?.checkbox || false,
+      date:
+        page.properties.Date?.date?.start || null,
+    }));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(tasks),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message,
+      }),
+    };
+  }
 };
